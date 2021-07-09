8092f3c961ff86f37c9fc3e63c839f4883e1c3a1
dinhKhoi-noob <khoip1305@gmail.com>
2021-07-09 08:54:59 +0700

    Completed Todo-app's function

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,9 @@
 const uri = "http://localhost:5000/api/"
 
+
 const changeStatus = async(id) => {
     const getTodoResponse = await fetch(uri+id);
-    const res = await getTodoResponse.json();        
+    const res = await getTodoResponse.json();
     const {name,status,deadline} = res.todo;
     const newContent = {
         name,
@@ -33,17 +34,112 @@ const changeStatus = async(id) => {
         })
     }
 }
-
-const onSubmitHandler = (event) =>{
-    const name = document.getElementById("name").value;
-    const deadline = document.getElementById("deadline").value;
-    const formEditor = document.getElementById("editForm");
-    if(formEditor.attachEvent){
-        form.attachEvent("submit",event.preventDefault())
+const toggleTaskEditor = async(id) =>{
+    try {
+        const response = await fetch(uri+id);
+        response.json().then((res)=>{
+            if(res.success){
+                document.getElementById("name").value = res.todo.name;
+                document.getElementById("deadline").value = res.todo.deadline;
+                status = res.todo.status;
+                const formEditor = document.getElementById("editForm");
+                formEditor.addEventListener("submit",async(event)=>{
+                    event.preventDefault();
+                    const name = document.getElementById("name").value;
+                    const deadline = document.getElementById("deadline").value;
+                    const newTaskContent = {
+                        name,
+                        deadline,
+                        status
+                    }
+                    const snapshot = await fetch(uri+id,{
+                        method:"PATCH",
+                        headers:{
+                            "Content-Type": "application/json"
+                        },
+                        body: JSON.stringify(newTaskContent)
+                    })
+                    snapshot.json()
+                    .then(res=>{
+                        if(res.success)
+                        {
+                            document.getElementById("sbmEditor").setAttribute("data-dismiss","modal");
+                            alert("Updated");
+                            window.location.reload();
+                        }
+                    })
+                    .catch(error=>{
+                        alert("Something was wrong");
+                        document.getElementById("sbmEditor").setAttribute("data-dismiss","modal");
+                        window.location.reload();
+                    })
+                })
+            }
+        });   
+    } 
+    catch (error) 
+    {
+        console.error(error);
     }
-    
 }
 
+const deleteTask = async id => {
+    const response = await fetch(uri+id,{
+        method:"DELETE",
+    })
+    response.json()
+    .then(res=>{
+        if(res.success)
+        {
+            alert("Deleted");
+            window.location.reload();
+        }
+    })
+    .catch(err=>{
+        alert("Something was wrong");
+        window.location.reload();
+    })
+}
+
+
+const addNewTask = async() => {
+    const formToggled = document.getElementById("add-new-form");
+    formToggled.addEventListener("submit",async(event)=>{
+        event.preventDefault();
+        const name = document.getElementById("new-name").value;
+        const deadline = document.getElementById("new-deadline").value;
+        const newPost = {
+            name,
+            deadline,
+            status:false
+        }
+        const response = await fetch(uri,{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify(newPost)
+        })
+        response.json()
+        .then(res=>{
+            if(res.success)
+            {
+                alert("Added new task");
+                document.getElementById("new-name").value = "";
+                document.getElementById("new-deadline").value = "";
+                document.getElementById("sbm-add-new").setAttribute("data-dismiss","modal");
+                window.location.reload();
+            }
+        })
+        .catch(err=>{
+            alert("Something was wrong");
+            document.getElementById("new-name").value = "";
+            document.getElementById("new-deadline").value = "";
+            document.getElementById("sbm-add-new").setAttribute("data-dismiss","modal");
+            window.location.reload();
+        })
+    })
+}
 
 const changeContent=async(id,name,status,deadline)=>{
     const response = await fetch(uri+id,{
